b222237d84104167fbad2a7dfaf67c3d670db758
Wonjeong Kim <38902021+NARUBROWN@users.noreply.github.com>
2023-11-20 14:27:32 +0900

    [init] 초기 커밋

---
+++ src/views/profileView.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+import informationCard from "@/components/profileView/informationCard.vue";
+</script>
+
+<template>
+  <informationCard/>
+</template>
+
+<style scoped>
+
+</style>
